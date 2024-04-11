818209531afceb5804bc5dc8e395334b42d9cfd6
Max50204 <schneidermaxim45@yahoo.com>
2024-04-11 11:16:30 +0200

    HW

---
+++ src/Char_Int.java
@@ -0,0 +1,19 @@
+public class Char_Int {
+    public static void main(String[] args) {
+        int num1 = 'A';
+        int num = '3';
+        int num2 = Integer.valueOf('A');
+        int num3 = Character.getNumericValue('A');
+        char ch = 65;
+        char ch1 = 127;
+
+        System.out.println(num1);
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(ch);
+        System.out.println(ch1);
+
+
+    }
+}
